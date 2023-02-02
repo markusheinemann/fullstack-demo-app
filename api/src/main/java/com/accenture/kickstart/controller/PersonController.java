3158702ab36bc91ddd4881aa94ea70cc74447809
Christoph Hoehn <christoph.hoehn@accenture.com>
2023-02-02 10:50:37 +0100

    add new interface

--- api/src/main/java/com/accenture/kickstart/controller/PersonController.java
+++ api/src/main/java/com/accenture/kickstart/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.accenture.kickstart.repository.PersonRepository;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,11 @@ public class PersonController {
     public Iterable<Person> listAll() {
         var people = personRepository.findAll();
         return people;
+    }
+
+    @GetMapping("/personID/{id}")
+    public Person getPersonById(@PathVariable("id") Integer itemOptionId){
+        return personRepository.findById(new Long(itemOptionId)).get();
     }
 
     @PostMapping("")
--- api/src/test/java/com/accenture/kickstart/DemoApiApplicationTests.java
+++ api/src/test/java/com/accenture/kickstart/DemoApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.accenture.kickstart;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApiApplicationTests {
 
     @Test
+    @DisplayName("Check if the jdbcTemplate object passed in the setter is set correctly.")
     void contextLoads() {
+
+        assertEquals(1,1);
     }
 
 }
